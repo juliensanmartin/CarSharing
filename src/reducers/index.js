549af17ee2cc0eb2af6c941b7e3ff33f5c11ef99
juliensanmartin <sanmartinjulien@gmail.com>
2016-06-15 08:30:23 -0700

    master:working boiler-plate code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+//import PostReducer from './reducer_post';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    state:(state = {}) => state
 });
 
 export default rootReducer;
