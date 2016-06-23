04d1cb848cfae233dd0cf04b3085d7f78b49ae6c
juliensanmartin <sanmartinjulien@gmail.com>
2016-06-23 14:47:04 -0700

    master:played a little with googlemap api refactor the map component with props

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -9,46 +9,68 @@ import { bindActionCreators } from 'redux';
 
 
 export default class MyGoogleMap extends Component {
+
     constructor(props){
         super(props);
 
         this.state = {
-            origin:{lat:49.2827, lng:-123.1207},
-            destination:{lat:49.222289, lng:-122.997237},
-            zoom:12,
+            origin:null,
+            destination:null,
             markers:[]
         };
 
         this.onMapClick = this.onMapClick.bind(this);
         this.retrieveDataFromGoogleMap = this.retrieveDataFromGoogleMap.bind(this);
 
+        this.service = new google.maps.DistanceMatrixService();
+
     }
 
-    onMapClick(event){
+    onMapClick(event) {
+        // if origin is null then we set origin otherwise we set destination
+        (!this.state.origin) ? this.state.origin = event.latLng : this.state.destination = event.latLng;
 
-        console.log('origin :'+this.state.origin);
-        console.log('destination :'+this.state.destination);
+        var originMarker = new google.maps.Marker({
+            position: this.state.origin,
+            animation: google.maps.Animation.DROP,
+            draggable:true,
+            title: 'From',
+            key: Date.now()
+        });
 
-        var origin = new google.maps.LatLng(this.state.origin);
-        var destination = new google.maps.LatLng(this.state.destination);
+        var destinationMarker = new google.maps.Marker({
+            position: this.state.destination,
+            animation: google.maps.Animation.DROP,
+            draggable:true,
+            title: 'To',
+            key: Date.now()+1
+        });
 
-        var service = new google.maps.DistanceMatrixService();
+        this.setState({markers:[originMarker, destinationMarker]});
 
-        const param = {
-            origins: [origin],
-            destinations: [destination],
-            travelMode: google.maps.TravelMode.DRIVING,
-            /*transitOptions: TransitOptions,*/
-            /*drivingOptions: DrivingOptions,*/
-            avoidHighways: false,
-            avoidTolls: false
-        };
+        if (this.state.destination) {
 
-        service.getDistanceMatrix(param, this.retrieveDataFromGoogleMap);
+            // create the param for the googlemap request
+            const param = {
+                origins: [this.state.origin],
+                destinations: [this.state.destination],
+                travelMode: google.maps.TravelMode.DRIVING,
+                /*transitOptions: TransitOptions,*/
+                /*drivingOptions: DrivingOptions,*/
+                avoidHighways: false,
+                avoidTolls: false
+            };
+
+            this.service.getDistanceMatrix(param, this.retrieveDataFromGoogleMap);
+        }
+
+
     }
 
     retrieveDataFromGoogleMap(response, status){
-        this.props.fetchDistanceMatrix(response);
+        if (status == google.maps.DistanceMatrixStatus.OK) {
+            this.props.fetchDistanceMatrix(response);
+        }
     }
 
     render() {
@@ -57,16 +79,31 @@ export default class MyGoogleMap extends Component {
                 containerElement={ <div className="container-google-map col-sd-12" /> }
                 googleMapElement={
                     <GoogleMap
-                        defaultZoom={this.state.zoom}
-                        defaultCenter={{lat: this.state.origin.lat, lng: this.state.origin.lng}}
+                        defaultZoom={this.props.zoom}
+                        defaultCenter={this.props.center}
                         onClick={this.onMapClick}>
-
+                            {this.state.markers.map((marker, index) => {
+                                return (
+                                    <Marker {...marker} />
+                                );
+                            })}
                     </GoogleMap>
                 }
             />
         );
     }
 }
+
+MyGoogleMap.propTypes = {
+    center : React.PropTypes.object.isRequired,
+    zoom : React.PropTypes.number.isRequired
+};
+
+MyGoogleMap.defaultProps = {
+    center: new google.maps.LatLng(49.2827, -123.1207),
+    zoom:12
+};
+
 
 
 function mapDispatchToProps(dispatch) {
